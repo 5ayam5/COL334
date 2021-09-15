6f52f48e2259a1fac12c07214085aaade1c719da
5ayam5 <sayam.sethi2@gmail.com>
2021-09-16 00:42:44 +0530

    assignment completed!

--- Assignments/Assignment 2/Client.java
+++ Assignments/Assignment 2/Client.java
@@ -1,10 +1,14 @@
 import java.io.*;
-import java.net.*;
+import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.Arrays;
 import java.util.List;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Client {
     private static class SockThread extends Thread {
+        protected static final int MAX_HEADER_LINE = 116, MAX_TRIES = 2;
         protected Socket socket;
         protected OutputStream send;
         protected InputStream recv;
@@ -15,21 +19,90 @@ public class Client {
            recv = socket.getInputStream();
         }
 
-        protected void sendData(List<String> headerList, String message) throws IOException {
-            send.write((String.join("\n", headerList) + "\n\n" + message).getBytes());
-            send.flush();
-            // @TODO receive response
+        protected String readLine() throws IOException {
+            StringBuilder ret = new StringBuilder();
+            char next = (char)recv.read();
+            if (next == 65535)
+                throw new IOException();
+            while (next != '\n' && ret.length() < MAX_HEADER_LINE) {
+                ret.append(next);
+                next = (char)recv.read();
+            }
+            if (next != '\n')
+                throw new IOException();
+            return ret.toString();
         }
 
-        protected void registerClient(String code) throws IOException {
-            while (true) {
+        protected ArrayList<String> getHeader() throws IOException {
+            ArrayList<String> ret = new ArrayList<>();
+            String next = readLine();
+            while (!next.equals("")) {
+                ret.add(next);
+                next = readLine();
+            }
+            return ret;
+        }
+
+        protected int parseResponse(ArrayList<String> headerList, String check) {
+            if (headerList.size() != 1)
+                return 103;
+
+            String[] header = headerList.get(0).split(" ");
+            switch (header[0]) {
+                case "REGISTERED":
+                    if (header.length != 3)
+                        return 103;
+                    if (!header[2].equals(check))
+                        return 103;
+                    switch (header[1]) {
+                        case "TOSEND":
+                            return 200;
+                        case "TORECV":
+                            return 201;
+                        default:
+                            return 103;
+                    }
+                case "SENT":
+                    if (header.length != 2)
+                        return 103;
+                    return header[1].equals(check) ? 202 : 103;
+                case "ERROR":
+                    if (header.length < 2)
+                        return 103;
+                    switch (header[1]) {
+                        case "100":
+                            return 100;
+                        case "101":
+                            return 101;
+                        case "102":
+                            return 102;
+                        default:
+                            return 103;
+                    }
+                default:
+                    return 103;
+            }
+        }
+
+        protected int sendData(List<String> headerList) throws IOException {
+            return sendData(headerList, "", username);
+        }
+
+        protected int sendData(List<String> headerList, String message, String check) throws IOException {
+            send.write((String.join("\n", headerList) + "\n\n" + message).getBytes());
+            send.flush();
+            return parseResponse(getHeader(), check);
+        }
+
+        protected int registerClient(String code) throws IOException {
+            for (int i = 0; i < MAX_TRIES; ++i) {
                 try {
-                    sendData(Arrays.asList(String.join(" ", "REGISTER", code, username)), "");
-                    break;
+                    return sendData(Arrays.asList(String.join(" ", "REGISTER", code, username)));
                 } catch(IOException e) {
                     continue;
                 }
             }
+            throw new IOException();
         }
 
         @Override
@@ -40,30 +113,139 @@ public class Client {
 
     private static class SendThread extends SockThread {
         public SendThread() throws IOException {
-            // empty constructor to handle throwing of exception
+            // empty constructor to handle throwing of IOException
+        }
+
+        private void inAndSend(Scanner scanner) throws IOException {
+            String target = scanner.next();
+            if (target.charAt(0) != '@' || target.length() == 1) {
+                System.out.println("No target user specified, please retype the message");
+                return;
+            }
+            target = target.substring(1);
+
+            String message = scanner.nextLine().substring(1);
+            ArrayList<String> headerList = new ArrayList<>();
+            headerList.add(String.format("SEND %s", target));
+            headerList.add(String.format("Content-length: %d", message.getBytes().length));
+
+            switch (sendData(headerList, message, target)) {
+                case 202:
+                    System.out.println("Sent!");
+                    return;
+                case 102:
+                    System.out.println("Could not send message");
+                    return;
+                default:
+                    throw new IOException();
+            }
         }
 
         @Override
         public void run() {
             try {
-                registerClient("TOSEND");
+                int code = registerClient("TOSEND");
+                if (code != 200) {
+                    System.err.println(String.format("Error registering for TOSEND, error code %d", code));
+                    throw new IOException();
+                }
+                Scanner scanner = new Scanner(System.in);
+                while (!isInterrupted())
+                    inAndSend(scanner);
+                throw new IOException();
             } catch (IOException e) {
-                return;
+                e.printStackTrace();
+                try {
+                    socket.close();
+                } catch(IOException e1) {
+                    return;
+                }
             }
-        }   
+        }
     }
 
     private static class RecvThread extends SockThread {
         public RecvThread() throws IOException {
-            // empty constructor to handle throwing of exception
+            // empty constructor to handle throwing of IOException
+        }
+
+        private void sendResponse(String header) throws IOException {
+            for (int i = 0; i < MAX_TRIES; ++i) {
+                try {
+                    send.write((header + "\n\n").getBytes());
+                    send.flush();
+                    return;
+                } catch(IOException e) {
+                    continue;
+                }
+            }
+            throw new IOException();
+        }
+
+        private void sendResponse(int errno) throws IOException {
+            sendResponse(errno, "");
+        }
+
+        private void sendResponse(int errno, String msg) throws IOException {
+            switch (errno) {
+                case 103:
+                    sendResponse("ERROR 103 Header incomplete");
+                    throw new IOException();
+                case 203:
+                    sendResponse(String.format("RECEIVED %s", msg));
+                    break;
+                default:
+                    break;
+            }
+        }
+        private void recvAndOut() throws IOException {
+            ArrayList<String> headerList = getHeader();
+            if (headerList.size() != 2)
+                sendResponse(103);
+
+            String[] line = headerList.get(0).split(" ");
+            if (line.length != 2 || !line[0].equals("FORWARD"))
+                sendResponse(103);
+            String sender = line[1];
+
+            line = headerList.get(1).split(" ");
+            if (line.length != 2 || !line[0].equals("Content-length:"))
+                sendResponse(103);
+
+            try {
+                int len = Integer.parseInt(line[1]);
+                byte[] message = new byte[len];
+                int curr = 0;
+                while (curr < len) {
+                    int read = recv.read(message, curr, len - curr);
+                    if (read != -1)
+                        curr += read;
+                }
+                System.out.println(String.format("%s: %s", sender, new String(message)));
+                sendResponse(203, sender);
+            } catch(NumberFormatException e) {
+                sendResponse(103);
+                throw new IOException();
+            }
         }
 
         @Override
         public void run() {
             try {
-                registerClient("TORECV");
+                int code = registerClient("TORECV");
+                if (code != 201) {
+                    System.err.println(String.format("Error registering for TORECV, error code %d", code));
+                    throw new IOException();
+                }
+                while (!isInterrupted())
+                    recvAndOut();
+                throw new IOException();
             } catch (IOException e) {
-                return;
+                try {
+                    socket.close();
+                } catch(IOException e1) {
+                    return;
+                }
             }
         }
     }
@@ -72,7 +254,7 @@ public class Client {
     private static String username = null, address = "localhost";
     private static int port = 2428; // stands for "chat"
 
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         if (args.length == 0) {
             System.err.println("Missing argument: <username> needed");
             return;
@@ -95,9 +277,12 @@ public class Client {
 
         try {
             SendThread sendThread = new SendThread();
-            sendThread.start();
             RecvThread recvThread = new RecvThread();
+            sendThread.start();
             recvThread.start();
+            while (sendThread.isAlive() && recvThread.isAlive());
+            sendThread.interrupt();
+            recvThread.interrupt();
         } catch (UnknownHostException e) {
             System.err.println("Invalid host given");
         } catch (IOException e) {
--- Assignments/Assignment 2/Server.java
+++ Assignments/Assignment 2/Server.java
@@ -1,15 +1,17 @@
 import java.io.*;
 import java.net.*;
+import java.util.Map;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.ArrayList;
 
 public class Server {
     private static class ClientThread extends Thread {
-        private static final int MAX_HEADER_LINE = 116;
+        private static final int MAX_HEADER_LINE = 116, MAX_TRIES = 2;
         private Socket socket;
-        private String username;
-        private OutputStream send;
+        protected String username;
+        protected OutputStream send;
         private InputStream recv;
 
         public ClientThread(Socket socket) throws IOException {
@@ -19,26 +21,56 @@ public class Server {
         }
 
         private static boolean registeredUser(String username) {
-            return recvSockets.containsKey(username) && sendUsers.contains(username);
+            return username.equals("ALL") || (recvSockets.containsKey(username) && sendUsers.contains(username));
         }
 
-        // @TODO implement this as thread (to make it consistent with broadcasting)
-        private static void sendData(Socket socket, ArrayList<String> headerList, String message) throws IOException {
-            socket.getOutputStream().write((String.join("\n", headerList) + "\n\n" + message).getBytes());
-            socket.getOutputStream().flush();
-            // @TODO receive response
+        private static void removeUser(String username) {
+            sendUsers.remove(username);
+            recvSockets.remove(username);
         }
 
-        private void sendError(int errno) throws IOException {
+        private void sendResponse(String header) throws IOException {
+            for (int i = 0; i < MAX_TRIES; ++i) {
+                try {
+                    send.write((header + "\n\n").getBytes());
+                    send.flush();
+                    return;
+                } catch(IOException e) {
+                    continue;
+                }
+            }
+            throw new IOException();
+        }
+
+        private void sendResponse(int errno) throws IOException, InterruptedException {
+            sendResponse(errno, "");
+        }
+
+        private void sendResponse(int errno, String msg) throws IOException, InterruptedException {
             switch (errno) {
                 case 100:
+                    sendResponse("ERROR 100 Malformed username");
                     break;
                 case 101:
+                    sendResponse("ERROR 101 No user registered");
                     break;
                 case 102:
+                    sendResponse("ERROR 102 Unable to send");
                     break;
                 case 103:
+                    sendResponse("ERROR 103 Header incomplete");
                     throw new IOException();
+                case 200:
+                    sendResponse("REGISTERED TOSEND " + msg);
+                    System.err.println(username + " connected for send");
+                    break;
+                case 201:
+                    sendResponse("REGISTERED TORECV " + msg);
+                    System.err.println(username + " connected for recv");
+                    throw new InterruptedException();
+                case 202:
+                    sendResponse("SENT " + msg);
+                    break;
                 default:
                     break;
             }
@@ -47,6 +79,8 @@ public class Server {
         private String readLine() throws IOException {
             StringBuilder ret = new StringBuilder();
             char next = (char)recv.read();
+            if (next == 65535)
+                throw new IOException();
             while (next != '\n' && ret.length() < MAX_HEADER_LINE) {
                 ret.append(next);
                 next = (char)recv.read();
@@ -56,8 +90,8 @@ public class Server {
             return ret.toString();
         }
 
-        private ArrayList<String> getHeader() throws IOException {
-            ArrayList<String> ret = new ArrayList<>();
+        protected List<String> getHeader() throws IOException {
+            List<String> ret = new ArrayList<>();
             String next = readLine();
             while (!next.equals("")) {
                 ret.add(next);
@@ -66,58 +100,187 @@ public class Server {
             return ret;
         }
 
-        private void connectClient(String[] header) throws IOException {
-            if (!header[0].equals("REGISTER")) {
-                sendError(103);
-                return;
-            }
+        private void connectClient() throws IOException, InterruptedException {
+            List<String> headerList = getHeader();
+            if (headerList.size() != 1)
+                sendResponse(103);
+            String[] header = headerList.get(0).split(" ");
+            if (header.length < 3 || !header[0].equals("REGISTER"))
+                sendResponse(103);
+
             username = header[2];
             if (header[1].equals("TOSEND")) {
                 if (sendUsers.contains(username) || header.length != 3) {
-                    sendError(100);
+                    sendResponse(100);
                     return;
                 }
                 sendUsers.add(username);
-                System.err.println(username + " connected for send");
+                sendResponse(200, username);
             } else if (header[1].equals("TORECV")) {
                 if (recvSockets.containsKey(username) || header.length != 3) {
-                    sendError(100);
+                    sendResponse(100);
                     return;
                 }
                 recvSockets.put(username, socket);
-                System.err.println(username + " connected for recv");
-                throw new IOException();
+                sendResponse(201, username);
             } else
-                sendError(103);
+                sendResponse(103);
         }
 
-        private void recvMessage(ArrayList<String> headerList) throws IOException {
+        private boolean forward(List<String> targets, List<String> headerList, String message) {
+            HashMap<String, SendThread> threads = new HashMap<>();
+            for (String target : targets)
+                try {
+                    SendThread thread = new SendThread(recvSockets.get(target), headerList, message);
+                    thread.start();
+                    threads.put(target, thread);
+                } catch (IOException e) {
+                    removeUser(target);
+                }
+            boolean success = true;
+            for (Map.Entry<String, SendThread> thread : threads.entrySet())
+                try {
+                    thread.getValue().join();
+                } catch (InterruptedException e) {
+                    removeUser(thread.getKey());
+                    success = false;
+                }
+            return success;
+        }
+        
+        private List<String> generateTargets(String target) {
+            List<String> targets = new ArrayList<>();
+            if (target.equals("ALL")) {
+                for (String user : sendUsers)
+                    if (registeredUser(user))
+                        targets.add(user);
+            } else
+                targets.add(target);
+            return targets;
+        }
+
+        private String readMessage(int len) throws IOException {
+            byte[] message = new byte[len];
+            int curr = 0;
+            while (curr < len) {
+                int read = recv.read(message, curr, len - curr);
+                if (read != -1)
+                    curr += read;
+            }
+            return new String(message);
+        }
+
+        private void recvAndFwdMsg() throws IOException, InterruptedException {
+            List<String> headerList = getHeader();
             if (!registeredUser(username)) {
-                sendError(101);
+                sendResponse(101);
                 return;
+            }
+            if (headerList.size() != 2)
+                sendResponse(103);
+
+            String[] line = headerList.get(0).split(" ");
+            if (line.length != 2 || !line[0].equals("SEND"))
+                sendResponse(103);
+
+            String target = line[1];
+            line = headerList.get(1).split(" ");
+            if (line.length != 2 || !line[0].equals("Content-length:"))
+                sendResponse(103);
+            if (!registeredUser(target)) {
+                try {
+                    readMessage(Integer.parseInt(line[1]));
+                    sendResponse(102);
+                    return;
+                } catch (NumberFormatException e) {
+                    sendResponse(103);
+                    throw new IOException();
+                }
+            }
+            List<String> targets = generateTargets(target);
+
+            try {
+                headerList.set(0, String.format("FORWARD %s", username));
+                if (forward(targets, headerList, readMessage(Integer.parseInt(line[1]))))
+                    sendResponse(202, target);
+                else
+                    sendResponse(102);
+            } catch(NumberFormatException e) {
+                sendResponse(103);
+                throw new IOException();
             }
         }
 
         @Override
         public void run() {
             try {
-                ArrayList<String> headerList = getHeader();
-                if (headerList.size() == 1)
-                    connectClient(headerList.get(0).split(" "));
-                else if (headerList.size() == 2)
-                    recvMessage(headerList);
-                else
-                    throw new IOException();
+                connectClient();
+                while (true)
+                    recvAndFwdMsg();
             } catch (IOException e) {
+                removeUser(username);
+                try {
+                    socket.close();
+                } catch(IOException e1) {
+                    return;
+                }
+            } catch (InterruptedException ie) {
                 return;
+            }
+        }
+    }
+
+    private static class SendThread extends ClientThread {
+        private List<String> headerList;
+        private String message;
+
+        public SendThread(Socket socket, List<String> headerList, String message) throws IOException {
+            super(socket);
+            this.headerList = headerList;
+            this.message = message;
+        }
+
+        private int parseResponse(List<String> headerList, String check) {
+            if (headerList.size() != 1)
+                return 103;
+
+            String[] header = headerList.get(0).split(" ");
+            if (header.length != 2)
+                return 103;
+            switch (header[0]) {
+                case "RECEIVED":
+                    return header[1].equals(check) ? 203 : 103;
+                default:
+                    return 103;
+            }
+        }
+
+        private int sendData(List<String> headerList, String message) throws IOException {
+            send.write((String.join("\n", headerList) + "\n\n" + message).getBytes());
+            send.flush();
+            headerList = getHeader();
+            return parseResponse(headerList, username);
+        }
+
+        @Override
+        public void run() {
+            try {
+                switch (sendData(headerList, message)) {
+                    case 203:
+                        return;
+                    default:
+                        throw new IOException();
+                }
+            } catch(IOException e) {
+                this.interrupt();
             }
         }
     }
 
     private static int port = 2428; // stands for "chat"
     private static ServerSocket serverSocket = null;
-    private static HashMap<String, Socket> recvSockets = new HashMap<>(); // map for TORECV sockets
-    private static HashSet<String> sendUsers = new HashSet<>(); // set of users added via TOSEND
+    private static volatile HashMap<String, Socket> recvSockets = new HashMap<>(); // map for TORECV sockets
+    private static volatile HashSet<String> sendUsers = new HashSet<>(); // set of users added via TOSEND
 
     // listen for new clients to join and create threads for handling them
     public static void connectionLoop() {
@@ -132,7 +295,7 @@ public class Server {
         }
     }
 
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         if (args.length > 0)
             try {
                 port = Integer.parseInt(args[0]);
